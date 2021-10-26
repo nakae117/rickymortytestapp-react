3bae443cda4223182a715e10353d2e61425fddaa
Edgar Carrizalez <edg.sistemas@gmail.com>
2021-10-25 21:10:23 -0400

    Primer Commit

---
+++ components/layouts/main.js
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+export default function Main({ children }) {
+	return (
+		<>
+			<main>
+				{/* <Container> */}
+					{children}
+				{/* </Container> */}
+			</main>
+		</>
+	)
+}
